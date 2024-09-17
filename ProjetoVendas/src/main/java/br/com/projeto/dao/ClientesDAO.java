a1ea686e0180029134149948f770fe62ac90977b
marcosalexper <marcosalexper@hotmail.com>
2024-09-17 11:33:59 -0300

    feat: adicionado metodo alterarCliente classe ClientesDAO

--- ProjetoVendas/src/main/java/br/com/projeto/dao/ClientesDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/ClientesDAO.java
@@ -19,125 +19,154 @@ import javax.swing.JOptionPane;
  * @author Marcos Alexandre Pereira
  */
 public class ClientesDAO {
-    
+
     private Connection con;
-    
-    public ClientesDAO(){
+
+    public ClientesDAO() {
         this.con = new ConnectionFactory().getConnection();
     }
-    
+
     //Metodo cadastrarCliente
-    public void cadastrarCliente(Clientes obj){
-        
+    public void cadastrarCliente(Clientes obj) {
+
         try {
-            
+
             //Criar comando sql
-            String sql = "insert into tb_clientes(nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"+
-                    "values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            
+            String sql = "insert into tb_clientes(nome,rg,cpf,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"
+                    + "values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
             //Conectar o banco de dados e organizar o comando sql
-             PreparedStatement stmt = con.prepareStatement(sql);
-             stmt.setString(1, obj.getNome());
-             stmt.setString(2, obj.getRg());
-             stmt.setString(3, obj.getCpf());
-             stmt.setString(4, obj.getEmail());
-             stmt.setString(5, obj.getTelefone());
-             stmt.setString(6, obj.getCelular());
-             stmt.setString(7, obj.getCep());
-             stmt.setString(8, obj.getEndereco());
-             stmt.setInt(9, obj.getNumero());
-             stmt.setString(10, obj.getComplemento());
-             stmt.setString(11, obj.getBairro());
-             stmt.setString(12, obj.getCidade());
-             stmt.setString(13, obj.getUf());
-             
-             //Executar o comando sql
-             stmt.execute();
-             stmt.close();
-             
-             JOptionPane.showMessageDialog(null,"Cadastrado com sucesso!");
-             
-            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+
         } catch (SQLException erro) {
-            JOptionPane.showMessageDialog(null,"Erro:" +erro);
-        
-        
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
         }
-        
+
     }
+
     //Metodo AlterarCliente
-    public void alterarCliente(){
-        
+    public void alterarCliente(Clientes obj) {
+        try {
+
+            //Criar comando sql
+            String sql = "update tb_clientes set nome=?,rg=?,cpf=?,"
+                    + "email=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getUf());
+            stmt.setInt(14, obj.getId());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
+        }
+
     }
+
     //Metodo ExcluirCliente
-    public void excluirCliente(Clientes obj){
-         try {
-             
-     
+    public void excluirCliente(Clientes obj) {
+        try {
+
             //Criar comando sql
             String sql = "delete from tb_clientes where id = ?";
-                   
-            
+
             //Conectar o banco de dados e organizar o comando sql
-             PreparedStatement stmt = con.prepareStatement(sql);
-             stmt.setInt(1, obj.getId());
-             
-             //Executar o comando sql
-             stmt.execute();
-             stmt.close();
-             
-             JOptionPane.showMessageDialog(null,"Excluido com sucesso!");
-             
-            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+
         } catch (SQLException erro) {
-            JOptionPane.showMessageDialog(null,"Erro:" +erro);
-        
-        
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
         }
-        
+
     }
+
     //Metodo Listar Todos Clientes
-    public List<Clientes> listarClientes(){
-        
+    public List<Clientes> listarClientes() {
+
         try {
-            
+
             //Criar a lista
             List<Clientes> lista = new ArrayList<>();
-           
+
             //Criar, organizar e executar o sql
             String sql = "select*from tb_clientes";
             PreparedStatement stmt = con.prepareStatement(sql);
-           ResultSet rs = stmt.executeQuery();
-           
-           while(rs.next()){
-               Clientes obj = new Clientes();
-               
-               obj.setId(rs.getInt("id"));
-               obj.setNome(rs.getString("nome"));
-               obj.setRg(rs.getString("rg"));
-               obj.setCpf(rs.getString("cpf"));
-               obj.setEmail(rs.getString("email"));
-               obj.setTelefone(rs.getString("telefone"));
-               obj.setCelular(rs.getString("celular"));
-               obj.setCep(rs.getString("cep"));
-               obj.setEndereco(rs.getString("endereco"));
-               obj.setNumero(rs.getInt("numero"));
-               obj.setComplemento(rs.getString("complemento"));
-               obj.setBairro(rs.getString("bairro"));
-               obj.setCidade(rs.getString("cidade"));
-               obj.setUf(rs.getString("estado"));
-               
-               lista.add(obj);
-           }
-           
-           return lista;
-            
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Clientes obj = new Clientes();
+
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                lista.add(obj);
+            }
+
+            return lista;
+
         } catch (Exception erro) {
-            
+
             JOptionPane.showMessageDialog(null, "Erro:" + erro);
             return null;
         }
     }
-    
-    
+
 }
