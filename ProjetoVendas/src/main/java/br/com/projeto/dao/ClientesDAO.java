e973126297243b62e3fcf17f96da118752195dc9
marcosalexper <marcosalexper@hotmail.com>
2024-09-17 15:20:04 -0300

    feat: adicionado metodo buscaClientePorNome na classe ClientesDAO

--- ProjetoVendas/src/main/java/br/com/projeto/dao/ClientesDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/ClientesDAO.java
@@ -168,5 +168,49 @@ public class ClientesDAO {
             return null;
         }
     }
+    //Metodo Buscar cliente por nome
+    public List<Clientes> buscaClientePorNome(String nome) {
+
+        try {
+
+            //Criar a lista
+            List<Clientes> lista = new ArrayList<>();
+
+            //Criar, organizar e executar o sql
+            String sql = "select*from tb_clientes where nome like ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Clientes obj = new Clientes();
+
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (Exception erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+            return null;
+        }
+    }
+    
 
 }
