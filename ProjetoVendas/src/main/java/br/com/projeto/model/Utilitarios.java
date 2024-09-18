d8049c4daac2a0bf49970ab3dc1e01fd76dddab7
marcosalexper <marcosalexper@hotmail.com>
2024-09-17 23:59:38 -0300

    feat: criada classe Utilitarios

---
+++ ProjetoVendas/src/main/java/br/com/projeto/model/Utilitarios.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto.model;
+
+/**
+ *
+ * @author Win11
+ */
+public class Utilitarios {
+    
+//Metodo limparCampos    
+    
+}
