1aac2a19e0167d82e882dbec386167977415a353
marcosalexper <marcosalexper@hotmail.com>
2024-09-18 00:05:30 -0300

    feat: adicionado metodo LimpaTela na classe Utilitarios

--- ProjetoVendas/src/main/java/br/com/projeto/model/Utilitarios.java
+++ ProjetoVendas/src/main/java/br/com/projeto/model/Utilitarios.java
@@ -4,12 +4,25 @@
  */
 package br.com.projeto.model;
 
+import java.awt.Component;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  *
  * @author Win11
  */
 public class Utilitarios {
     
-//Metodo limparCampos    
+//Metodo limparCampos  
+public void LimpaTela(JPanel container){
+    Component components[] = container.getComponents();
+    for(Component component : components){
+        if(component instanceof JTextField){
+            ((JTextField) component).setText(null);
+        }
+    }
+}    
+    
     
 }
