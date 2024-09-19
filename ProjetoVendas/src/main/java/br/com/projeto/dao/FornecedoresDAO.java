71c2f094d717081578f70724b98f1bb2310ea8a6
marcosalexper <marcosalexper@hotmail.com>
2024-09-19 15:01:12 -0300

    feat: criado metodos excluir e alterar Fornecedores na classe FornecedoresDAO

--- ProjetoVendas/src/main/java/br/com/projeto/dao/FornecedoresDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/FornecedoresDAO.java
@@ -13,16 +13,17 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Win11
+ * @author Marcos Alexandre Pereira
  */
 public class FornecedoresDAO {
-    
-        private Connection con;
+
+    private Connection con;
 
     public FornecedoresDAO() {
         this.con = new ConnectionFactory().getConnection();
     }
-       //Metodo cadastrarFornecedores
+    //Metodo cadastrarFornecedores
+
     public void cadastrarFornecedores(Fornecedores obj) {
 
         try {
@@ -56,7 +57,67 @@ public class FornecedoresDAO {
             JOptionPane.showMessageDialog(null, "Erro:" + erro);
 
         }
+    }
+
+    //Metodo excluir Fornecedor
+    public void excluirFornecedor(Fornecedores obj) {
+        try {
+
+            //Criar comando sql
+            String sql = "delete from tb_fornecedores where id = ?";
+
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Excluido com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
+        }
 
     }
     
+    //Metodo alterar Fornecedor
+     public void alterarFornecedor(Fornecedores obj) {
+        try {
+
+            //Criar comando sql
+            String sql = "update tb_fornecedores set nome=?,cnpj=?,"
+                    + "email=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getCnpj());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setString(5, obj.getCelular());
+            stmt.setString(6, obj.getCep());
+            stmt.setString(7, obj.getEndereco());
+            stmt.setInt(8, obj.getNumero());
+            stmt.setString(9, obj.getComplemento());
+            stmt.setString(10, obj.getBairro());
+            stmt.setString(11, obj.getCidade());
+            stmt.setString(12, obj.getUf());
+            stmt.setInt(13, obj.getId());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
+        }
+
+    }
+
 }
