b49abdd478adc42d2fc3e4828012043cd5269559
marcosalexper <marcosalexper@hotmail.com>
2024-09-19 11:51:38 -0300

    feat: criado classes Fornecedores e FornecedoresDAO

---
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/FornecedoresDAO.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto.dao;
+
+import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Fornecedores;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Win11
+ */
+public class FornecedoresDAO {
+    
+        private Connection con;
+
+    public FornecedoresDAO() {
+        this.con = new ConnectionFactory().getConnection();
+    }
+       //Metodo cadastrarFornecedores
+    public void cadastrarFornecedores(Fornecedores obj) {
+
+        try {
+
+            //Criar comando sql
+            String sql = "insert into tb_fornecedores(nome,cnpj,email,telefone,celular,cep,endereco,numero,complemento,bairro,cidade,estado)"
+                    + "values(?,?,?,?,?,?,?,?,?,?,?,?)";
+
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getCnpj());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setString(5, obj.getCelular());
+            stmt.setString(6, obj.getCep());
+            stmt.setString(7, obj.getEndereco());
+            stmt.setInt(8, obj.getNumero());
+            stmt.setString(9, obj.getComplemento());
+            stmt.setString(10, obj.getBairro());
+            stmt.setString(11, obj.getCidade());
+            stmt.setString(12, obj.getUf());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
+        }
+
+    }
+    
+}
---
+++ ProjetoVendas/src/main/java/br/com/projeto/model/Fornecedores.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.projeto.model;
+
+/**
+ *
+ * @author Win11
+ */
+public class Fornecedores extends Clientes {
+    
+    private String cnpj;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    
+    
+    
+}
