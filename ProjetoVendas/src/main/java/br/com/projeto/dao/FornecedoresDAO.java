957c5d064a02d00bf9ad39d201d05177df66b1c8
marcosalexper <marcosalexper@hotmail.com>
2024-09-19 15:07:31 -0300

    feat: criado metodo listar Fornecedores

--- ProjetoVendas/src/main/java/br/com/projeto/dao/FornecedoresDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/FornecedoresDAO.java
@@ -8,7 +8,10 @@ import br.com.projeto.jdbc.ConnectionFactory;
 import br.com.projeto.model.Fornecedores;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -118,6 +121,48 @@ public class FornecedoresDAO {
 
         }
 
+    }
+     
+     //Metodo Listar Todos Fornecedores
+    public List<Fornecedores> listarFornecedores() {
+
+        try {
+
+            //Criar a lista
+            List<Fornecedores> lista = new ArrayList<>();
+
+            //Criar, organizar e executar o sql
+            String sql = "select*from tb_fornecedores";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Fornecedores obj = new Fornecedores();
+
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));             
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (Exception erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+            return null;
+        }
     }
 
 }
