7b245af52dd790d3a2b54b3e93fbe2a799a27527
marcosalexper <marcosalexper@hotmail.com>
2024-09-20 15:25:26 -0300

    feat: criado metodos Alterar e Excluir produtos na classe ProdutosDAO

--- ProjetoVendas/src/main/java/br/com/projeto/dao/ProdutosDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/ProdutosDAO.java
@@ -50,41 +50,87 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "Erro..." + erro);
         }
     }
-    //Metodo listar Produtos
-    public List<Produtos>listarProdutos(){
+
+    //Metodo alterar Produtos
+    public void alterar(Produtos obj) {
+        try {
+
+            String sql = "update into tb_produtos set descricao=?,preco=?,qtd_estoque=?,for_id=? where id=?";
+
+            //Conectar banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getDescricao());
+            stmt.setDouble(2, obj.getPreco());
+            stmt.setInt(3, obj.getQtd_estoque());
+            stmt.setInt(4, obj.getFornecedor().getId());
+
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Produto alterado com sucesso!");
+
+        } catch (Exception erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro..." + erro);
+        }
+
+    }
+    
+    //Metodo excluir Produtos
+    public void excluir(Produtos obj){
         try {
             
-            List<Produtos> lista = new ArrayList<>();
+            String sql = "delete from tb_produtos where id=?";
             
+            PreparedStatement stmt = con.prepareStatement(sql);
+            
+            stmt.setInt(1,obj.getId());
+            
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null,"Produto excluido com sucesso!");
+            
+        } catch (Exception erro) {
+            
+            JOptionPane.showMessageDialog(null,"Erro..." +erro);
+        }
+    }
+
+    //Metodo listar Produtos
+    public List<Produtos> listarProdutos() {
+        try {
+
+            List<Produtos> lista = new ArrayList<>();
+
             //Criar, organizar e executar o sql
             String sql = "select p.id, p.descricao, p.preco, p.qtd_estoque, f.nome from tb_produtos as p "
                     + "inner join tb_fornecedores as f on(p.for_id = f.id)";
 
-                         
             PreparedStatement stmt = con.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 Produtos obj = new Produtos();
                 Fornecedores f = new Fornecedores();
-                
+
                 obj.setId(rs.getInt("p.id"));
                 obj.setDescricao(rs.getString("p.descricao"));
                 obj.setPreco(rs.getDouble("p.preco"));
                 obj.setQtd_estoque(rs.getInt("p.qtd_estoque"));
-                
+
                 f.setNome(rs.getString("f.nome"));
-                
+
                 obj.setFornecedor(f);
-                
+
                 lista.add(obj);
-                
+
             }
             return lista;
-            
+
         } catch (SQLException erro) {
-            
-            JOptionPane.showMessageDialog(null,"Erro..." +erro);
+
+            JOptionPane.showMessageDialog(null, "Erro..." + erro);
             return null;
         }
     }
