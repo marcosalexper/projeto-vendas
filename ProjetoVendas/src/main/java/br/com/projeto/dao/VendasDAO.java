5efdb734a413ac98b41e75eb12222ba91e6258a8
marcosalexper <marcosalexper@hotmail.com>
2024-09-23 11:19:22 -0300

    feat: criado o metodo listarVendasPorPeriodo

--- ProjetoVendas/src/main/java/br/com/projeto/dao/VendasDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/VendasDAO.java
@@ -5,11 +5,14 @@
 package br.com.projeto.dao;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
 import br.com.projeto.model.Vendas;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,9 +20,9 @@ import javax.swing.JOptionPane;
  * @author Marcos Alexandre Pereira
  */
 public class VendasDAO {
-
+    
     private Connection con;
-
+    
     public VendasDAO() {
         this.con = new ConnectionFactory().getConnection();
     }
@@ -33,7 +36,7 @@ public class VendasDAO {
 
             //Conectar o banco de dados e organizar o comando sql
             PreparedStatement stmt = con.prepareStatement(sql);
-
+            
             stmt.setInt(1, obj.getCliente().getId());
             stmt.setString(2, obj.getData_venda());
             stmt.setDouble(3, obj.getTotal_venda());
@@ -42,12 +45,10 @@ public class VendasDAO {
             //Executar o comando sql
             stmt.execute();
             stmt.close();
-
-  
-
+            
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro:" + erro);
-
+            
         }
     }
 
@@ -55,25 +56,65 @@ public class VendasDAO {
     public int retornaUltimaVenda() {
         try {
             int idvenda = 0;
-
+            
             String sql = "select max(id)id from tb_vendas";
             PreparedStatement ps = con.prepareStatement(sql);
-
+            
             ResultSet rs = ps.executeQuery();
-
+            
             if (rs.next()) {
                 Vendas p = new Vendas();
-
+                
                 p.setId(rs.getInt("id"));
-
+                
                 idvenda = p.getId();
             }
             return idvenda;
-
+            
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
+        
     }
 
+    //Metodo que filtra Vendas por Datas
+    public List<Vendas> listarVendasPorPeriodo(String data_inicio, String data_fim) {
+        try {
+            
+            List<Vendas> lista = new ArrayList<>();
+
+            //Criar, organizar e executar o sql
+            String sql = "select v.id, v.data_venda, c.nome, v.total_venda, v.observacoes from tb_vendas as v "
+                    + " inner join tb_clientes as c on(v.cliente id=c.id)where v.data_venda BETWEEN ? AND ?";
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, data_inicio);
+            stmt.setString(2, data_fim);
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Vendas obj = new Vendas();
+                Clientes c = new Clientes();
+                
+                obj.setId(rs.getInt("v.id"));
+                obj.setData_venda(rs.getString("v.data_venda"));
+                c.setNome(rs.getString("c.nome"));
+                obj.setTotal_venda(rs.getDouble("v.total_venda"));
+                obj.setObs(rs.getString("v.observacoes"));
+                
+                obj.setCliente(c);
+                
+                lista.add(obj);
+                
+            }
+            return lista;
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro..." + erro);
+            return null;
+        }
+    }    
+    
 }
