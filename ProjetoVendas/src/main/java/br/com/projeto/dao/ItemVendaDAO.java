a1666e24f06a458a691e239b1ddce96a48d70bb1
marcosalexper <marcosalexper@hotmail.com>
2024-09-22 21:09:07 -0300

    feat: criado o método cadastraItem em ItemVendaDAO

--- ProjetoVendas/src/main/java/br/com/projeto/dao/ItemVendaDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/ItemVendaDAO.java
@@ -5,7 +5,11 @@
 package br.com.projeto.dao;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.ItemVenda;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,5 +23,31 @@ public class ItemVendaDAO {
         this.con = new ConnectionFactory().getConnection();
     }
     
+    //Metodo que cadastra Itens
+    public void cadastraItem(ItemVenda obj){
+        try {
+            //Criar comando sql
+            String sql = "insert into tb_itensvendas(venda_id,produto_id,qtd,subtotal)"
+                    + "values(?,?,?,?)";
+
+            //Conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setInt(1, obj.getVenda().getId());
+            stmt.setInt(2, obj.getProduto().getId());
+            stmt.setInt(3, obj.getQtd());
+            stmt.setDouble(4, obj.getSubtotal());
+
+            //Executar o comando sql
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Item registrada com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+
+        } 
+    }
     
 }
