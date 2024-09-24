91b83626b66d6bfc61fe8e820f1b72fdac7fc59f
marcosalexper <marcosalexper@hotmail.com>
2024-09-24 00:35:42 -0300

    fix: corrigido comando sql que retorna os dados da venda para a FrmDetalheVenda

--- ProjetoVendas/src/main/java/br/com/projeto/dao/ItemVendaDAO.java
+++ ProjetoVendas/src/main/java/br/com/projeto/dao/ItemVendaDAO.java
@@ -23,9 +23,9 @@ import javax.swing.JOptionPane;
  * @author Marcos Alexandre Pereira
  */
 public class ItemVendaDAO {
-    
+
     private Connection con;
-    
+
     public ItemVendaDAO() {
         this.con = new ConnectionFactory().getConnection();
     }
@@ -39,7 +39,7 @@ public class ItemVendaDAO {
 
             //Conectar o banco de dados e organizar o comando sql
             PreparedStatement stmt = con.prepareStatement(sql);
-            
+
             stmt.setInt(1, obj.getVenda().getId());
             stmt.setInt(2, obj.getProduto().getId());
             stmt.setInt(3, obj.getQtd());
@@ -48,51 +48,50 @@ public class ItemVendaDAO {
             //Executar o comando sql
             stmt.execute();
             stmt.close();
-            
+
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro:" + erro);
-            
-        }        
+
+        }
     }
 
     //Metodo que lista itens de uma venda por id
     public List<ItemVenda> listaItensPorVenda(int venda_id) {
-        
+
         List<ItemVenda> lista = new ArrayList<>();
-        
+
         try {
 
             //Criar, organizar e executar o sql
-            String query = "select i.id, p.descrica, i.qtd, p.preco, i.subtotal from tb_itensvenda as i "
-                    + " inner join tb_produtos as p on(i,produto_id = p.id) where i.venda_id = ?";
-            
+            String query = "select p.descricao, i.qtd, p.preco, i.subtotal from tb_itensvendas as i "
+                    + " inner join tb_produtos as p on(i.produto_id = p.id) where i.venda_id = ?";
+
             PreparedStatement ps = con.prepareStatement(query);
             ps.setInt(1, venda_id);
-            
+
             ResultSet rs = ps.executeQuery();
-            
+
             while (rs.next()) {
                 ItemVenda item = new ItemVenda();
                 Produtos prod = new Produtos();
-                
-                item.setId(rs.getInt("i.id"));
+
                 prod.setDescricao(rs.getString("p.descricao"));
-                item.setQtd(rs.getInt("i.qtdl"));
+                item.setQtd(rs.getInt("i.qtd"));
                 prod.setPreco(rs.getDouble("p.preco"));
                 item.setSubtotal(rs.getDouble("i.subtotal"));
-                
+
                 item.setProduto(prod);
-                
+
                 lista.add(item);
-                
+
             }
             return lista;
-            
+
         } catch (SQLException erro) {
-            
+
             JOptionPane.showMessageDialog(null, "Erro..." + erro);
             return null;
         }
-    }    
-    
+    }
+
 }
